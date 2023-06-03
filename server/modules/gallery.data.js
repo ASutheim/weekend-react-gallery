f002af1bc21c5197e752e1605f7f95950878d88c
Anna Sutheim <asutheim@gmail.com>
2023-06-03 09:08:14 -0500

    Sourced photos in to gallery file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,40 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/family.jpg",
+    description:
+      "My family! Mom, dad, godmother, brother, me. This was the last family photo we took while my dad was alive.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "images/homebackyard.jpg",
+    description: "Where I grew up, in northeast Los Angeles.",
+  },
+  { id: 3, path: "images/maryandkids", description: "My partner and her kids" },
+  {
+    id: 4,
+    path: "images/maryandmecamping",
+    description: "My partner and me on a camping trip last fall",
+  },
+  {
+    id: 5,
+    path: "images/jermsandcat",
+    description: "My brother snuggling my cat. I love them both a lot.",
+  },
+  {
+    id: 6,
+    path: "images/dobostorte",
+    description:
+      "A Hungarian cake (dobos torte) I made for my dad. I love baking for loved ones.",
+  },
+  {
+    id: 7,
+    path: "images/solveigportrait",
+    description:
+      "A small kid I love drew this picture of me in chalk. I think it is a very accurate portrait.",
+  },
+  // { id: 8, path: 'images/', description: ''},
 ];
 
 module.exports = galleryItems;
